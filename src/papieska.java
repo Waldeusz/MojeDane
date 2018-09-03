f6ee37dee5f37a7f5708b976e5cda0fdb5a0e646
Waldeusz <wdzwigalski@gmail.com>
2018-09-03 18:01:45 +0200

    Dodanie programu Papieska

---
+++ src/papieska.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class papieska {
+	public static void main(String[] args){
+	  Scanner papieska = new Scanner(System.in);
+	 
+
+
+	  int papiez = 0;
+	do
+	 {
+		 System.out.println("Podaj liczbê: ");
+		 papiez = papieska.nextInt();
+		 
+		 
+	 }
+	 while(papiez !=2137);
+	 
+	 System.out.println("PAPIESKA!");
+
+  }
+}
